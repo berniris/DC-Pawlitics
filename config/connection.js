365862c230e1dcd387b49d2b861dce58045fccc6
berniris <berniriscoleman@gmail.com>
2018-08-11 06:05:09 -0400

    Deploying app

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
 const config = require('./dbConfig');
 
-const db = pgp(config);
+const db = pgp(process.env.DATABASE_URL) || config); // config is the configuration object
 module.exports = db;
